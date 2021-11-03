8e0cc7576131b5f0d82ed51998f7b7205b6273b6
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-11-03 21:43:25 +0530

    deleted bundles temporarily

--- lib/index.d.ts
+++
@@ -1,7 +0,0 @@
-declare type text = string;
-declare type int = number;
-declare type charCodes = number[];
-declare function convertToAscii(expression: text): charCodes;
-declare let checkAscii: charCodes;
-declare function convertIntoToChar(...args: number[]): string;
-//# sourceMappingURL=index.d.ts.map
