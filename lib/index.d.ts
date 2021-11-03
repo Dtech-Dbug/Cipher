d2e22049f322262d20a54aa476e010de88d37722
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-11-03 23:10:22 +0530

    bundled js cd=ode

---
+++ lib/index.d.ts
@@ -0,0 +1,6 @@
+declare type text = string;
+declare type int = number;
+declare type charCodes = number[];
+declare function convertToAscii(expression: text): charCodes;
+declare function convertIntegerToChar(...args: number[]): string;
+//# sourceMappingURL=index.d.ts.map
